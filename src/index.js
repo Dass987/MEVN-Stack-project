1897b52e81a3bfc25879d67f2de406c37f0eb231
Danniel Sequera <sequeradanniel@gmail.com>
2019-01-09 23:29:54 -0400

    Primer commit del MEVN Stack project

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const app = express();
+
+mongoose.connect('mongodb://localhost/mevn-database')
+	.then(res => console.log('DB is connected'))
+	.catch(err => console.log(err));
+
+// --- Settings
+app.set('port', process.env.PORT || 3000);
+
+// --- Middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+
+// --- Routes
+app.use('/tasks', require('./routes/tasks'));
+
+// --- Static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// --- Server is listening
+app.listen(app.get('port'), () => {
+	console.log('Server on port ' + app.get('port'));
+});
---
+++ src/routes/tasks.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+const Task = require('./../models/Task');
+
+router.get('/', async (request, response) => {
+	const tasks = await Task.find();
+	response.json(tasks);
+});
+
+router.post('/', async (request, response) => {	
+	console.log('PASO POR AQUIIIIII');
+	const task = new Task(request.body);
+	await task.save();
+	response.json({status: 'Task Saved'});
+});
+
+router.put('/:id', async (request, response) => {
+	await Task.findByIdAndUpdate(request.params.id, request.body);
+	response.json({status: 'Task Updated'});
+});
+
+router.delete('/:id', async (request, response) => {
+	await Task.findByIdAndDelete(request.params.id);
+	response.json({status: 'Task Deleted'});
+});
+
+module.exports = router;
